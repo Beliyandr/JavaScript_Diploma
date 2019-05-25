7d86a4fa5b13d66f9173cb726ce8937b76d53102
Andrey <beliyandr@gmail.com>
2019-05-25 21:19:27 +0300

    correction

---
+++ src/parts/images.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+let section = document.querySelector('.work');
+
+
+
+
+
+});
--- src/parts/modal.js
+++ src/parts/modal.js
@@ -1,48 +1,51 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-let modalPopup = document.querySelector('.popup'),
-    modalEngineer = document.querySelector('.popup_engineer'),
-    btnEngineer = document.querySelector('.popup_engineer_btn'),
-    phoneLink = document.querySelector('.phone_link'),
-    closeForm = document.querySelectorAll('.popup_close'),
-    timerPopup = setInterval(openModalPopup, 60000);
+  let modalPopup = document.querySelector('.popup'),
+      modalEngineer = document.querySelector('.popup_engineer'),
+      btnEngineer = document.querySelector('.popup_engineer_btn'),
+      phoneLink = document.querySelectorAll('.phone_link'),
+      closeForm = document.querySelectorAll('.popup_close'),
+      timerPopup = setTimeout(openModalPopup, 5000);
 
-    function openModalPopup(popup = modalPopup) {
-      popup.style.display = 'block';
+  function openModalPopup(popup = modalPopup) {
+    popup.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  }
+
+  phoneLink.forEach(function(item,i) {
+    phoneLink[i].addEventListener('click', function () {
+    event.preventDefault();
+    openModalPopup(modalPopup);
+  })
+});
+
+  btnEngineer.addEventListener('click', function (e) {
+    openModalPopup(modalEngineer);
+  });
+
+  /// CLOSE FORM //////
+  function closeForms() {
+    modalPopup.style.display = '';
+    modalEngineer.style.display = '';
+    document.body.style.overflow = '';
+  }
+
+  closeForm.forEach(function (item, i) {
+    closeForm[i].addEventListener('click', function () {
+      closeForms();
+    });
+  })
+
+  modalPopup.addEventListener('click', (e) => {
+    if (e.target.classList.contains("popup")) {
+      closeForms();
     }
+  })
 
-phoneLink.addEventListener('click', function () {
-  openModalPopup(modalPopup);
-});
-
-btnEngineer.addEventListener('click', function () {
-  openModalPopup(modalEngineer);
-});
-
-
-/// CLOSE FORM //////
-function closeForms() {
-  modalPopup.style.display = '';
-  modalEngineer.style.display = '';
-}
-
-closeForm.forEach(function (item,i) { 
-  closeForm[i].addEventListener('click', function() { 
-    closeForms();
-});
-})
-
-modalPopup.addEventListener('click', (e) => {
-  if (e.target.classList.contains("popup")){
-    closeForms();
-  }
-}) 
-
-modalEngineer.addEventListener('click', (e) => {
-  if (e.target.classList.contains("popup_engineer")){
-    closeForms();
-  }
-}) 
-///// END CLOSE FORM //////
-
+  modalEngineer.addEventListener('click', (e) => {
+    if (e.target.classList.contains("popup_engineer")) {
+      closeForms();
+    }
+  })
+  ///// END CLOSE FORM //////
 });
