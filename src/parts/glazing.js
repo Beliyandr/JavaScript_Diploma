bd70a2c4403a504a7835a9352704e437ed5ebff4
Andrey <beliyandr@gmail.com>
2019-05-27 18:55:11 +0300

    revision

--- src/parts/glazing.js
+++ src/parts/glazing.js
@@ -1,16 +1,17 @@
 window.addEventListener('DOMContentLoaded', function () {
 let glazing = document.querySelector('.glazing_slider'),
     elemGlaz = glazing.querySelectorAll('.glazing_block'),
-    linkGlaz = document.querySelectorAll('.glazing_block a');
+    linkGlaz = document.querySelectorAll('.glazing_block a'),
+    row = document.querySelectorAll('.glazing .row');
     
     elemGlaz.forEach(function(item, i) {
     elemGlaz[i].addEventListener('click', function() {
         for (let j = 0 ; j < elemGlaz.length; j++) {
             linkGlaz[j].classList.remove('active');
+            row[j].style.display = 'none';
         }
         linkGlaz[i].classList.add('active');
+        row[i].style.display = 'block';
     });
 });
-
-
 });
